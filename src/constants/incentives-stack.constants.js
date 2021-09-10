282af16ae668fde603854d8cb22633e27fd778c4
Ekeu Ulrich <ulrich.ekeu.monkam@efrei.net>
2021-09-10 16:23:11 +0200

    Added incentives, cta and updated link buttons

---
+++ src/constants/incentives-stack.constants.js
@@ -0,0 +1,22 @@
+export const INCENTIVES_STACK = [
+  {
+    name: 'Strapi',
+    imageSrc: 'strapi.svg',
+    description: 'Backend',
+  },
+  {
+    name: 'Gatsby',
+    imageSrc: 'gatsby.svg',
+    description: 'Frontend',
+  },
+  {
+    name: 'MongoDB',
+    imageSrc: 'mongodb.svg',
+    description: 'Data Storage',
+  },
+  {
+    name: 'Stripe',
+    imageSrc: 'stripe.svg',
+    description: 'Payment',
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,16 @@ import Layout from '../components/layout/layout.component'
 import Hero from '../components/home/hero/hero.component'
 import PromoProducts from '../components/home/promo-products/promo-products.component'
 import FeaturedProducts from '../components/home/featured-products/featured-products.component'
+import IncentivesStack from '../components/home/incentives-stack/incentives-stack.component'
+import Cta from '../components/home/cta/cta.component'
 
 const IndexPage = () => (
   <Layout>
     <Hero />
     <PromoProducts />
+    <IncentivesStack />
     <FeaturedProducts />
+    <Cta />
   </Layout>
 )
 
